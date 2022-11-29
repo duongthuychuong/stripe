2afe8eabb5f4cbc970e13eaf764fb93079ba5b4b
chuongduong <duongthuychuong@gmail.com>
2022-11-29 21:43:44 +0700

    general content

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,25 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
-
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalState } from "./context";
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { setIsSubmenuOpen } = useGlobalState;
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images"></article>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
