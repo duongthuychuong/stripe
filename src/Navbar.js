044aee62494e86cfc2f1be9647a9d9104eb49506
chuongduong <duongthuychuong@gmail.com>
2022-11-29 21:38:53 +0700

    nav bar setup

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
-
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalState } from "./context";
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const { setIsSidebarOpen, setIsSubmenuOpen } = useGlobalState();
 
-export default Navbar
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="" />
+          <button
+            className="btn toggle-btn"
+            onClick={() => {
+              setIsSidebarOpen(true);
+            }}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li className="link-btn">products</li>
+          <li className="link-btn">developers</li>
+          <li className="link-btn">company</li>
+          <li className="link-btn">company</li>
+        </ul>
+        <button className="btn signin-btn">Sign in</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
